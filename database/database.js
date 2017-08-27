865483fc73d9123890f7069ccd1fcbb2d6f44e9f
Valentine <zavrak.valentin@gmail.com>
2017-08-27 22:57:48 +0300

    domain changed

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
 const config = require('../config/config.json')[env];
 
-mongoose.connect(`mongodb://${config.DOMAIN}/${config.DATABASE_NAME}`, {
+mongoose.connect(`mongodb://localhost/${config.DATABASE_NAME}`, {
   useMongoClient: true,
 });
 
