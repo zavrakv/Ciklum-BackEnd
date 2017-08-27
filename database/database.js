9563ba6897c484d94bffa2918ed62fafd9038113
Valentine <zavrak.valentin@gmail.com>
2017-08-27 20:09:43 +0300

    database connect changed

--- database/database.js
+++ database/database.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
 const config = require('../config/config.json')[env];
-const autoIncrement = require('mongoose-auto-increment');
 
-const connection = mongoose.connect(`mongodb://${config.DOMAIN}/${config.DATABASE_NAME}`, {
+mongoose.connect(`mongodb://${config.DOMAIN}/${config.DATABASE_NAME}`, {
   useMongoClient: true,
 });
 
-autoIncrement.initialize(connection);
-
-module.exports = { mongoose, autoIncrement };
+module.exports = { mongoose };
